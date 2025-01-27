d943d953ec717a7f87a93b5e475e944c0e90eca2
Makoto Ishibashi <arsoncase@gmail.com>
2025-01-27 15:54:52 +0900

    Add ComparatorFactory

---
+++ src/domain/factory/ComparatorFactory.ts
@@ -0,0 +1,21 @@
+import Comparator from "../comparator/Comparator";
+import Equal from "../comparator/Equal";
+import GreaterThanOrEqual from "../comparator/GreaterThanOrEqual";
+import LessThanOrEqual from "../comparator/LessThanOrEqual";
+
+class ComparatorFactory {
+  static create(type: string): Comparator {
+    switch (type) {
+      case "以上？":
+        return new GreaterThanOrEqual();
+      case "以下？":
+        return new LessThanOrEqual();
+      case "？":
+        return new Equal();
+      default:
+        throw new Error("Invalid type");
+    }
+  }
+}
+
+export default ComparatorFactory;
